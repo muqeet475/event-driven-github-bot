78562518da0c661b644e1d8eec894cdac2c0ed4d
Abdul Muqeet <am7842169@gmail.com>
2026-06-30 21:50:05 +0530

    Update GitHub OAuth callback for Render deployment

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -6,7 +6,8 @@ passport.use(
     {
       clientID: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      callbackURL: "http://localhost:5000/auth/github/callback",
+      callbackURL:
+        "https://event-driven-github-bot.onrender.com/auth/github/callback",
     },
     function (accessToken, refreshToken, profile, done) {
       profile.accessToken = accessToken;
